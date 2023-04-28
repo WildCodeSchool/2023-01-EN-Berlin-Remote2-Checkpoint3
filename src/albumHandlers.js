15024a55257117707962a57178d9ddef4ee0513d
George Surmava <111701072+surmavagit@users.noreply.github.com>
2023-04-28 17:53:08 +0200

    POST albums

--- src/albumHandlers.js
+++ src/albumHandlers.js
@@ -27,8 +27,47 @@ const getAlbumById = (req, res) => {
       res.status(500).send('Error retrieving data from the database');
     });
 };
+const deleteAlbumById = (req, res) => {
+  const id = parseInt(req.params.id);
+  database
+    .query('delete from album where id = ?', [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('Not Found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error deleting the album');
+    });
+};
+const postAlbum = (req, res) => {
+  const { title, genre, picture, artist } = req.body;
+  database
+    .query(
+      'insert into album (title, genre, picture, artist) values (?, ?, ?, ?)',
+      [title, genre, picture, artist]
+    )
+    .then(([result]) => {
+      res.location(`/api/albums/${result.insertId}`).status(201).json({
+        id: result.insertId,
+        title: title,
+        genre: genre,
+        picture: picture,
+        artist: artist,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error saving the album');
+    });
+};
 
 module.exports = {
   getAlbums,
   getAlbumById,
+  postAlbum,
+  deleteAlbumById,
 };
--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const albumHandlers = require('./albumHandlers');
 
 app.get('/api/albums', albumHandlers.getAlbums);
 app.get('/api/albums/:id', albumHandlers.getAlbumById);
+app.post('/api/albums', albumHandlers.postAlbum);
+app.delete('/app/albums/:id', albumHandlers.deleteAlbumById);
 
 const trackHandlers = require('./trackHandlers');
 
